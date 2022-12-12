f8f5e2e3170194e88d09df42abda498fe4f3ffa1
Georg Schramm <gschra2@stanford.edu>
2022-12-12 12:33:36 -0800

    clean up bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,73 +4,28 @@ setlocal EnableDelayedExpansion
 mkdir build
 cd build
 
-echo "####################################################"
-echo "####################################################"
-echo "####### cuda_compiler_version#######################"
-echo "%cuda_compiler_version%"
-echo "####################################################"
-echo "####################################################"
-echo "####################################################"
-echo ""
-
-
-::if "%cuda_compiler_version%"=="None" (
-::    set EXTRA_CMAKE_ARGS="-DSKIP_CUDA_LIB=TRUE"
-::) else (
-::    set EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all"
-::)
-
-echo "####################################################"
-echo "####################################################"
-echo "####### PATH #######################################"
-echo %PATH%
-echo "####################################################"
-echo "####################################################"
-echo "####################################################"
-echo ""
-
-echo "####################################################"
-echo "####################################################"
-echo "####################################################"
-:: print path without duplicates
-python -c "import os;print(';'.join(list(dict.fromkeys(os.environ['PATH'].split(';')))))"
-echo "####################################################"
-echo "####################################################"
-echo "####################################################"
-
+:: for cuda build, the PATH variable gets too long resultung in commands that are to long
+:: to be executed
+:: we use a simple python oneliner and a temporary file to remove duplicate entries from PATH to make it shorter
 python -c "import os;print(';'.join(list(dict.fromkeys(os.environ['PATH'].split(';')))))">sanitized_path.txt
 for /f "delims=" %%x in (sanitized_path.txt) do set PATH=%%x
-
-echo "####################################################"
-echo "####################################################"
-echo "#### SANITIZED PATH ################################"
-type sanitized_path.txt
-echo "####################################################"
-echo "####################################################"
-echo "####################################################"
-
 
 :: Configure using the CMakeFiles
 
 if "%cuda_compiler_version%"=="None" (
-cmake -G Ninja ^
-    %CMAKE_ARGS% ^
-    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
-    -DCMAKE_BUILD_TYPE:STRING=Release ^
-    -DSKIP_DOCS=TRUE ^
-    -DSKIP_CUDA_LIB=TRUE ^
-    %SRC_DIR%
+    set EXTRA_CMAKE_ARGS="-DSKIP_CUDA_LIB=TRUE"
 ) else (
+    set EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all"
+)
+
 cmake -G Ninja ^
     %CMAKE_ARGS% ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DSKIP_DOCS=TRUE ^
-    -DCMAKE_CUDA_ARCHITECTURES=all ^
+    %EXTRA_CMAKE_ARGS% ^
     %SRC_DIR%
-)
 
 if %ERRORLEVEL% neq 0 exit 1
 
