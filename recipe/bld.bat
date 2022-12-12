2a8865c7eea864a975d02f03189d3f0bcf8f2f2a
Georg Schramm <gschra2@stanford.edu>
2022-12-12 06:54:01 -0800

    check PATH in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -11,18 +11,19 @@ echo "%cuda_compiler_version%"
 echo "####################################################"
 echo "####################################################"
 echo "####################################################"
+echo ""
 
 
-if "%cuda_compiler_version%"=="None" (
-    set EXTRA_CMAKE_ARGS="-DSKIP_CUDA_LIB=TRUE"
-) else (
-    set EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all"
-)
+::if "%cuda_compiler_version%"=="None" (
+::    set EXTRA_CMAKE_ARGS="-DSKIP_CUDA_LIB=TRUE"
+::) else (
+::    set EXTRA_CMAKE_ARGS="-DCMAKE_CUDA_ARCHITECTURES=all"
+::)
 
 echo "####################################################"
 echo "####################################################"
-echo "####### CMAKE EXTRA ARGS ###########################"
-echo %EXTRA_CMAKE_ARGS%
+echo "####### PATH #######################################"
+echo %PATH%
 echo "####################################################"
 echo "####################################################"
 echo "####################################################"
@@ -30,14 +31,27 @@ echo "####################################################"
 
 
 :: Configure using the CMakeFiles
+
+if "%cuda_compiler_version%"=="None" (
 cmake -G Ninja ^
     %CMAKE_ARGS% ^
     -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
     -DCMAKE_BUILD_TYPE:STRING=Release ^
     -DSKIP_DOCS=TRUE ^
-    %EXTRA_CMAKE_ARGS% ^
+    -DSKIP_CUDA_LIB=TRUE ^
     %SRC_DIR%
+) else (
+cmake -G Ninja ^
+    %CMAKE_ARGS% ^
+    -DCMAKE_INSTALL_PREFIX:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_PREFIX_PATH:PATH="%LIBRARY_PREFIX%" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DSKIP_DOCS=TRUE ^
+    -DCMAKE_CUDA_ARCHITECTURES=all
+    %SRC_DIR%
+)
+
 if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --target install --verbose 
